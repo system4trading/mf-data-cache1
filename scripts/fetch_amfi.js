d81b047f4d1b133f56af86ad6b831e8bdf28b83d
system4trading <104557852+system4trading@users.noreply.github.com>
2025-12-15 18:12:14 +0530

    Update fetch_amfi.js

--- scripts/fetch_amfi.js
+++ scripts/fetch_amfi.js
@@ -1,6 +1,29 @@
-import fs from 'fs';
-import fetch from 'node-fetch';
+import fs from "fs";
+import fetch from "node-fetch";
 
-const res = await fetch("https://www.amfiindia.com/spages/NAVAll.txt");
-fs.writeFileSync("raw.txt", await res.text());
+const AMFI_URL = "https://www.amfiindia.com/spages/NAVAll.txt";
 
+const text = await fetch(AMFI_URL).then(r => r.text());
+const lines = text.split("\n");
+
+const schemes = {};
+lines.forEach(l => {
+  const p = l.split(";");
+  if (p.length > 4 && !isNaN(p[0])) {
+    const code = p[0];
+    const nav = parseFloat(p[4]);
+    const date = p[7];
+    if (!schemes[code]) schemes[code] = [];
+    schemes[code].push({ date, nav });
+  }
+});
+
+fs.mkdirSync("amfi", { recursive: true });
+Object.keys(schemes).forEach(code => {
+  fs.writeFileSync(
+    `amfi/nav_${code}.json`,
+    JSON.stringify(schemes[code], null, 2)
+  );
+});
+
+console.log("AMFI NAV data updated");
